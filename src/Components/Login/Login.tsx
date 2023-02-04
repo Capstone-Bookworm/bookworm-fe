1c1eb531b38553570385b86c610bb3adee261775
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-02-04 11:18:53 -0700

    Add state and function to handle the change of the inputs

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import { useState } from 'react'
 import './Login.css'
 
 const Login = () => {
+  const [email, setEmail] = useState('')
+  const [username, setUsername] = useState('')
 
   const verifyLogin = () => {
-    console.log('hello');
+    console.log(email);
+    console.log(username);
+    
   }
 
   return (
     <section className='login-page'>
       <h1>BookWorm</h1>
       <div className='login-container'>
+        <h2>Login/ Create Account</h2>
         <input 
           className='email-login' 
           placeholder='Email'
-          value={'Email'}
+          value={email}
+          onChange={event => setEmail(event.target.value)}
         />
-        <input className='username-login' placeholder='Username'></input>
-        <button className='login-btn' onClick={event => verifyLogin()}>Login</button>
+        <input 
+          className='username-login' 
+          placeholder='Username'
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+          />
+        <button className='login-btn' onClick={verifyLogin}>Login</button>
       </div>
     </section>
   )
