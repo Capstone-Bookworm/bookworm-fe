d0df6ed0e1296530828ebb311a899dbe58f9c855
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-02-04 10:49:27 -0700

    Create routes and conditional rendering for navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom'
+
+interface Location {
+  pathname: string,
+  search: string,
+  hash: string,
+  state: null,
+  key: string
+}
 
 function App() {
+  let location: Location = useLocation()
   return (
-    <div className="App">
+    <div>
+      {location.pathname !== '/' && <h1>Header</h1>}
+      <Routes>
+        <Route path='/'/>
+        <Route path='/home'/>
+        <Route path='/details' />
+        <Route path='/myaccount' />
+        <Route path='/add-book' />
+      </Routes>
     </div>
   );
 }
