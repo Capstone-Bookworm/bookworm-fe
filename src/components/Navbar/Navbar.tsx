c828bcc02de73685f18b1357865d1e0bb9e3c6eb
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-02-04 12:36:12 -0700

    Add hamburger menu buttons

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { NavLink } from 'react-router-dom'
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx"
+
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState<boolean>(false)
+
+  const handleClick = () => {
+    setNavbarOpen(!navbarOpen)
+  }
+
+  const closeMenu = () => {
+    setNavbarOpen(false)
+  }
+
   return(
     <header className='navbar'>
       <section className='header'>
@@ -11,10 +23,11 @@ const Navbar = () => {
       </section>
       <br />
       <nav>
-        <ul className='buttons'>
-          <li><NavLink to='/home'><button>Home</button></NavLink></li>
-          <li><NavLink to='/myaccount'><button>My Account</button></NavLink></li>
-          <li><NavLink to='/'><button>Logout</button></NavLink></li>
+      <button onClick={handleClick}>{navbarOpen ? <RxHamburgerMenu /> : <RxCross2 />}</button>
+        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+          <li><NavLink to='/home'><button onClick={() => closeMenu()}>Home</button></NavLink></li>
+          <li><NavLink to='/myaccount'><button onClick={() => closeMenu()}>My Account</button></NavLink></li>
+          <li><NavLink to='/'><button onClick={() => closeMenu()}>Logout</button></NavLink></li>
         </ul>
       </nav>
     </header>
