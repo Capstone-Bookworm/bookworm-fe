c46fc26b759edb86ec0dda2d4ea9a7b2e685c099
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-02-07 15:47:23 -0700

    Query for search bar

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -4,17 +4,14 @@ import './Home.css'
 import { RxCaretRight, RxCaretLeft } from "react-icons/rx"
 import { useQuery, gql } from '@apollo/client'
 
-// const SEARCH_BOOKS = gql`
-//   mutation searchBooks {
-//     searchBooks (limit: 10) {
-//       title
-//       imageURL
-//       ISBN
-//       author
-//       pageCount
-//       summary
-//     }
-//   }`
+const BOOK_SEARCH = gql`
+  query BookSearch($title: String!) {   
+    bookSearch(title: $title) {
+        id
+        title
+        imageUrl
+      }
+  }`
 
 const BOOKS_DATA = gql `
     query books {
@@ -31,11 +28,10 @@ interface book {
 
 const Home: React.FC = () => {
   const [searchBook, setSearchBook] = useState<string>('')
-  const [limit, setLimit] = useState<number>(5)
   const [searchMessage, setSearchMessage] = useState<string>('')
   const style = { fontSize: "5em", cursor: 'pointer' }
   const { data } = useQuery(BOOKS_DATA)
-  const [bookData, setBookData] = useState(data.books.slice(5, 10))
+  const [bookData, setBookData] = useState(data.books)
 
   const bookList: JSX.Element[] = bookData.map((book: any) => {
     return (
@@ -47,13 +43,13 @@ const Home: React.FC = () => {
       )
     })
     
-    const handleClick = () => {
-    // const { loading, error, data } = useQuery(SEARCH_BOOKS)
-    // setBookData(data)
-    // const filterSearch = bookData.filter((book: any) => book.title.toLowerCase().includes(searchBook.toLowerCase()))
-    // setBookData(filterSearch)
-    setSearchMessage(`Search results for "${searchBook}". Please try a more specific search if your book is not displayed below.`)
-    // setSearchBook('')
+    const searchQuery = useQuery(BOOK_SEARCH, {
+      variables: { title: searchBook }})
+
+    const searchBooks = () => {
+      setBookData(searchQuery.data.bookSearch)
+      setSearchMessage(`Search results for "${searchBook}". Please try a more specific search if your book is not displayed below.`)
+      setSearchBook('')
   }
 
   const displayMoreBooks = (event: React.MouseEvent<HTMLElement>) => {
@@ -79,7 +75,7 @@ const Home: React.FC = () => {
           placeholder='Search by book title...'
           onChange={event => setSearchBook(event.target.value)}
         />
-        <button type='button' onClick={handleClick}>SEARCH</button>
+        <button type='button' onClick={() => searchBooks()}>SEARCH</button>
       </form>
       <h2 className='search-message'>{searchMessage}</h2>
       <div className={`book-container ${bookData.length > 5 ? "display-search" : ""}`}>
