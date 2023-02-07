e6da031607cacab779fc3d5899008e8f572d148a
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-02-07 16:52:56 -0700

    Add section for a search form and display container

--- src/Components/AddBook/AddBook.tsx
+++ src/Components/AddBook/AddBook.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
+import { setSyntheticLeadingComments } from 'typescript';
 import './AddBook.css'
 
 const AddBook = () => {
+  const [titleSearch, setTitle] = useState('')
+
+  console.log(titleSearch)
+  
   return (
     <section className='add-book-page'>
-      <h1>Hello There</h1>
+      <form className='search-form'>
+        <input
+          type='title'
+          className='search-input'
+          placeholder='Search by Title'
+          value={titleSearch}
+          onChange={event => setTitle(event.target.value)}
+        />
+      </form>
+      <div className='books-container'>
+
+      </div>
     </section>
   )
 }
