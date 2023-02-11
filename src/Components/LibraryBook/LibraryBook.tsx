f381a099dc9e14a420d0ee91e20435ea511a74fa
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-02-11 10:12:27 -0700

    Add basic styling to my books view

--- src/Components/LibraryBook/LibraryBook.tsx
+++ src/Components/LibraryBook/LibraryBook.tsx
@@ -10,11 +10,17 @@ interface UserLibraryBook {
 }
 
 const LibraryBook = (props: UserLibraryBook) => {
+
+  const deleteBook = (id: number) => {
+    console.log(id)
+    
+  }
+
   return(
-    <div className="book-card">
-      <img src={props.imageUrl} className={props.availability ? 'book-available': 'book-unavailable'} />
-      <h3>{props.title}</h3>
-      <button className="delete-btn">Delete From Library</button>
+    <div className="my-book-card">
+      <img src={props.imageUrl} alt={`Image of ${props.title}`} className={props.availability ? 'book-available': 'book-unavailable'} />
+      <h3 className="book-title">{props.title}</h3>
+      <button className="delete-btn" onClick={() => deleteBook(props.id)}>Delete From Library</button>
     </div>
   )
 }
