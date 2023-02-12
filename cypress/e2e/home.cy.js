5a699ef39d141075c9aec590afc5aadbc333fc7d
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-02-12 09:11:36 -0700

    WIP Home page testing

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,81 +1,54 @@
-describe('Login Page spec', () => {
+describe('Home page flow', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/') 
-    // cy.intercept('https://bookworm-be.herokuapp.com/graphql',
-    // cy.stub()
-    //   .callsFake(req => req.reply({ fixture: 'user.json' }))).as('userLogin')
-    // cy.wait('@books')
-    //   .visit('http://localhost:3000/home') 
-  });
-  it('Should be able to sign in', () => {
-    cy.get('.login-container').should('have.text', 'Book Worm')
-    cy.get('.email-login').type('lauren@gmail.com').should('have.value', 'lauren@gmail.com')
+    cy.visit('http://localhost:3000/')
+    cy.get('.email-login').type('lauren@gmail.com')
     cy.get('.create-acct-form > .login-btn').click()
-    // cy.wait('@userLogin')
-    //INTERCEPT POST
-    cy.visit('http://localhost:3000/home') 
+    cy.location("pathname").should("eq", "/home")
   })
-  it('Should have an option to create an account if one doesnt exist', () => {
-    cy.get('.create-acct').should('have.text', "Don't have an account? Create New Account")
-    cy.visit('http://localhost:3000/');
-    cy.get('.create-acct > .login-btn').click();
-    cy.get('.email-login').type('eleanor@gmail.com').should('have.value', 'eleanor@gmail.com')
-    cy.get('.username-login').type('eleanor').should('have.value', 'eleanor')
-    cy.get('.location-login').type('Denver, CO').should('have.value', 'Denver, CO')
-    // cy.get('.create-account-btn').click()
-    //INTERCEPT POST
-    cy.get('.login-btn').click()
-    cy.get('.email-login').should('have.value', 'eleanor@gmail.com')
+  it('Should expect to see a title and a logo with a nav bar', () => {
+    cy.get('.logo').should('have.attr', 'src', 'https://cdn-icons-png.flaticon.com/512/2789/2789786.png')
+    cy.get('.title').should('have.text', 'Bookworm')
+    cy.get('.welcome-display > h2').should('have.text', 'Welcome Lauren!')
+    cy.get('nav > button').click()
+    cy.get('.menuNav').should('contain', 'Home')
+      .and('contain', 'My Dashboard')
+      .and('contain', 'Logout')
+    cy.get('nav > button').click()
   })
-  it('Should throw an error if user account does not exist', () => {
-    cy.get('.email-login').type('eleanor@gmail.com').should('have.value', 'eleanor@gmail.com')
-    cy.get('.create-acct-form > .login-btn').click()
-    cy.get('.login-container > h3').should('have.text', "We couldn't find your account, please try again")
+  it('Should be able to see the display of all available books', () => {
+    cy.get('.book-list').should('be.visible').children().should('have.length', 7)
+    cy.get('.book-list').scrollTo('right', { easing: 'linear' })
+    cy.get(':nth-child(1) > a > .book-item-image')
+    cy.get(':nth-child(2) > a > .book-item-image')
+    cy.get(':nth-child(3) > a > .book-item-image')
+    cy.get(':nth-child(4) > a > .book-item-image')
+    cy.get(':nth-child(5) > a > .book-item-image')
+    cy.get(':nth-child(6) > a > .book-item-image')
+    cy.get(':nth-child(7) > a > .book-item-image')
   })
+  it('Should be able to search through the library of available books', () => {
+    cy.get('input').type('crying').should('have.value', 'crying')
+    // cy.get('.search-button').click() -- for some reason, the test isn't working...
+  })
+  it('Should be able to click on a book to see more details', () => {
+    //INTERCEPT POST REQUEST FOR BOOK DETAILS
+    cy.get(':nth-child(5) > a > .book-item-image').click()
+    cy.location("pathname").should("eq", "/details/26")
+    cy.get('.book-details-image').should('have.attr', 'src', 'http://books.google.com/books/content?id=PAMODAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api')
+    cy.get('#title').should('have.text', 'Grit by Angela Duckworth')
+    cy.get('#summary') // need to figure out how to detail with <br /> in cypress testing
+      // cy.get('#summary').should('have.text', 'Summary: This new edition of the acclaimed bestseller')
+    cy.get('#pages').should('have.text', '352 pages')
+    cy.get('.borrow-selection').select('Adelle')
+    // cy.get('#borrow-btn').click() // INTERCEPT POST(PATCH) REQUEST FOR BORROWING A BOOK
+  })
+
 })
-// })
 
 
-// describe('Excersises List Page', () => {
-//   beforeEach(() => {
-//     cy.intercept('POST', 'http://localhost:4000/graphql', (req) => {
-//       req.reply({
-//         statusCode: 200,
-//         body: MOCK_RESPONSES[req.body.operationName],
-//       });
-//     });
-//   });
 
-// })
-
-// cy.intercept('POST', 'https://bookworm-be.herokuapp.com/graphql', (req) => {
-  //   aliasMutation(req, 'userLogin')
-  
-  //   if (hasOperationName(req, 'userLogin')) {
-    //     req.alias = 'login'
-    
-    //     req.reply({
-      //       fixture: 'user.json'
-      //     })
-      //   }
-      // })
-
-      // cy.intercept('https://bookworm-be.herokuapp.com/graphql',
-      // cy.stub()
-      //   .callsFake(req => req.reply({ fixture: 'user.json' }))).as('userLogin')
-      // cy.wait(`@userLogin ${emailAddress}`)
-      // cy.visit('http://localhost:3000/home') 
-
-      // cy.intercept(
-      //   {
-      //     method: 'POST',
-      //     url: 'https://bookworm-be.herokuapp.com/graphql',
-      //     headers: {
-      //       'query': 'userLogin',
-      //     }
-      //   },
-      //   {
-      //     fixture: 'user.json'
-      //   }
-      // ).as('userLogin');
-      // x-gql-operation-name
+// I should see nav bar and logo (and menu)
+// I should see books
+// I should be able to search books and see results
+// I should be able to click on a book
+// I should see a error message if the server is down
