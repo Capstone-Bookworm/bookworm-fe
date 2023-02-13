b95332c4a1d6aa2af639b1a98db5b16ce85cf52a
Lauren Frazier <lauren.e.frazier@gmail.com>
2023-02-13 09:38:06 -0700

    WIP cypress testing with intercepts

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,20 @@
 describe('Home page flow', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/') 
+    cy.intercept({ method: "POST", url: "https://bookworm-be.herokuapp.com/graphql" }, (req) => {
+      if (req.body.operationName.includes("userLogin")) {
+        req.reply({ fixture: "user.json" });
+      }
+      if (req.body.operationName.includes("books")) {
+        req.reply({ fixture: "bookData.json" });
+      }
+      if (req.body.operationName.includes("bookSearch")) {
+        req.reply({ fixture: "bookSearch.json" })
+      }
+      // if (req.body.operationName.includes("user")) {
+      //   req.reply({ fixture: "myBooks.json" })
+      // }
+    })
     cy.get('.email-login').type('lauren@gmail.com')
     cy.get('.create-acct-form > .login-btn').click()
   })
@@ -15,24 +29,20 @@ describe('Home page flow', () => {
     cy.get('nav > button').click()
   })
   it('Should be able to see the display of all available books', () => {
-    cy.get('.book-list').should('be.visible').children().should('have.length', 7)
-    cy.get('.book-list').scrollTo('right', { easing: 'linear' })
+    cy.get('.book-list').should('be.visible').children().should('have.length', 2)
+    // cy.get('.book-list').scrollTo('right', { easing: 'linear' })
     cy.get(':nth-child(1) > a > .book-item-image')
     cy.get(':nth-child(2) > a > .book-item-image')
-    cy.get(':nth-child(3) > a > .book-item-image')
-    cy.get(':nth-child(4) > a > .book-item-image')
-    cy.get(':nth-child(5) > a > .book-item-image')
-    cy.get(':nth-child(6) > a > .book-item-image')
-    cy.get(':nth-child(7) > a > .book-item-image')
   })
-  it('Should be able to search through the library of available books', () => {
-    cy.get('input').type('crying').should('have.value', 'crying')
-    // cy.get('.search-button').click() -- for some reason, the test isn't working...
-    // INTERCEPT POST REQUEST
-  })
+  // it('Should be able to search through the library of available books', () => {
+  //   cy.get('input').type('witches').should('have.value', 'witches')
+  //   cy.get('.search-button').click() //-- for some reason, the test isn't working...
+  //   // INTERCEPT POST REQUEST
+  // })
+  //PICK BACK UP HERE
   it('Should be able to click on a book to see more details', () => {
     //INTERCEPT POST REQUEST FOR BOOK DETAILS
-    cy.get(':nth-child(5) > a > .book-item-image').click()
+    cy.get(':nth-child(1) > a > .book-item-image').click()
     cy.location("pathname").should("eq", "/details/26")
     cy.get('.book-details-image').should('have.attr', 'src', 'http://books.google.com/books/content?id=PAMODAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api')
     cy.get('#title').should('have.text', 'Grit by Angela Duckworth')
@@ -42,30 +52,30 @@ describe('Home page flow', () => {
     cy.get('.borrow-selection').select('Adelle')
     // cy.get('#borrow-btn').click() // INTERCEPT POST(PATCH) REQUEST FOR BORROWING A BOOK
   })
-  it('Should be able to navigate to their dashboard and see their personal library of books', () => {
-    cy.get('nav > button').click()
-    cy.get('.menuNav').should('contain', 'My Dashboard').click()
-    cy.get('nav > button').click()
-    cy.location("pathname").should("eq", "/dashboard")
-    cy.get('.user-dash-nav').should('contain', 'My Books')
-      .and('contain', 'My borrowed books')
-      .and('contain', 'Pending Requests')
-      .and('contain', 'Add a book')
-      cy.get('.user-book-welcome').should('have.text', "Lauren's Books")
-      cy.get('.my-books-container').should('be.visible').children().should('have.length', 3)
-      cy.get('.my-books-container > :nth-child(1)').should('have.text', 'Tuesdays with Morrie')
-        .find('img').should('have.attr', 'src', 'http://books.google.com/books/content?id=z2z_6hLoPmgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api')
-        .and('have.css', 'opacity', '0.5')
-      cy.get('.my-books-container > :nth-child(2)').should('have.text', 'Sins and Cigarettes')
-        .find('img').should('have.attr', 'src', 'http://books.google.com/books/content?id=XU4WxwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api')
-        .and('have.css', 'opacity', '0.5')
-      cy.get('.my-books-container > :nth-child(3)').should('have.text', 'The Seven Husbands of Evelyn Hugo')
-        .find('img').should('have.attr', 'src', 'http://books.google.com/books/content?id=5KlizgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api')
-        .and('have.css', 'opacity', '0.5')
-  })
-  it('Shold be able to logout of account', () => {
-    cy.get('nav > button').click()
-    cy.get('[href="/"] > li').click()
-    cy.location("pathname").should("eq", "/")
-  })
+  // it('Should be able to navigate to their dashboard and see their personal library of books', () => {
+  //   cy.get('nav > button').click()
+  //   cy.get('.menuNav').should('contain', 'My Dashboard').click()
+  //   cy.get('nav > button').click()
+  //   cy.location("pathname").should("eq", "/dashboard")
+  //   cy.get('.user-dash-nav').should('contain', 'My Books')
+  //     .and('contain', 'My borrowed books')
+  //     .and('contain', 'Pending Requests')
+  //     .and('contain', 'Add a book')
+  //     cy.get('.user-book-welcome').should('have.text', "Lauren's Books")
+  //     cy.get('.my-books-container').should('be.visible').children().should('have.length', 3)
+  //     cy.get('.my-books-container > :nth-child(1)').should('have.text', 'Tuesdays with Morrie')
+  //       .find('img').should('have.attr', 'src', 'http://books.google.com/books/content?id=z2z_6hLoPmgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api')
+  //       .and('have.css', 'opacity', '0.5')
+  //     cy.get('.my-books-container > :nth-child(2)').should('have.text', 'Sins and Cigarettes')
+  //       .find('img').should('have.attr', 'src', 'http://books.google.com/books/content?id=XU4WxwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api')
+  //       .and('have.css', 'opacity', '0.5')
+  //     cy.get('.my-books-container > :nth-child(3)').should('have.text', 'The Seven Husbands of Evelyn Hugo')
+  //       .find('img').should('have.attr', 'src', 'http://books.google.com/books/content?id=5KlizgEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api')
+  //       .and('have.css', 'opacity', '0.5')
+  // })
+  // it('Shold be able to logout of account', () => {
+  //   cy.get('nav > button').click()
+  //   cy.get('[href="/"] > li').click()
+  //   cy.location("pathname").should("eq", "/")
+  // })
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -20,19 +20,19 @@ describe('Login Page spec', () => {
     cy.get('.email-login').type('adelle@gmail.com').should('have.value', 'adelle@gmail.com')
     cy.get('.create-acct-form > .login-btn').click()
   })
-  it('Should have an option to create an account if one doesnt exist', () => {
-      cy.get('.create-acct').should('have.text', "Don't have an account? Create New Account")
-      cy.get('.create-acct > .login-btn').click();
-      cy.get('.email-login').type('eleanor@gmail.com').should('have.value', 'eleanor@gmail.com')
-      // trying to type courtney in after but it's not clearing...?
-      cy.get('.username-login').type('eleanor').should('have.value', 'eleanor')
-      cy.get('.location-login').type('Denver, CO').should('have.value', 'Denver, CO')
-      // cy.get('.create-account-btn').click()
-      //INTERCEPT POST
-      cy.get('.email-login').type('courtney@gmail.com').should('have.value', 'courtney@gmail.com')
-      cy.get('.create-acct-form > .login-btn').click()
-      // cy.get('.email-login').should('have.value', 'courtney@gmail.com')
-    })
+  // it('Should have an option to create an account if one doesnt exist', () => {
+  //     cy.get('.create-acct').should('have.text', "Don't have an account? Create New Account")
+  //     cy.get('.create-acct > .login-btn').click();
+  //     cy.get('.email-login').type('eleanor@gmail.com').should('have.value', 'eleanor@gmail.com')
+  //     // trying to type courtney in after but it's not clearing...?
+  //     cy.get('.username-login').type('eleanor').should('have.value', 'eleanor')
+  //     cy.get('.location-login').type('Denver, CO').should('have.value', 'Denver, CO')
+  //     // cy.get('.create-account-btn').click()
+  //     //INTERCEPT POST
+  //     cy.get('.email-login').type('courtney@gmail.com').should('have.value', 'courtney@gmail.com')
+  //     cy.get('.create-acct-form > .login-btn').click()
+  //     // cy.get('.email-login').should('have.value', 'courtney@gmail.com')
+  //   })
     // it('Should throw an error if user account does not exist', () => {
     //     cy.get('.email-login').type('eleanor@gmail.com').should('have.value', 'eleanor@gmail.com')
     //     cy.get('.create-acct-form > .login-btn').click()
